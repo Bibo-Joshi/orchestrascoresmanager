4cf67dedef18a65081db5c880925d26165dbf81e
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2026-07-08 21:36:14 +0200

    Make Router History Independent of App Path Settings
    Allows it to handle NC configs with and without index.php in the URL

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from './navigation'
+import { generateUrl } from '@nextcloud/router'
 
 const router = createRouter({
-	history: createWebHistory('/index.php/apps/orchestrascoresmanager/'),
+	history: createWebHistory(generateUrl('/apps/orchestrascoresmanager')),
 	routes,
 })
 
